e144b764f20164027ca3c3c7f0ddf96a9bb29772
youdontknowmeaf <core.contact@yahoo.com>
2026-06-03 08:27:59 +0200

    Added install script to the Raylib/Standard release.

---
+++ RayLib/Standard/build/CMakeFiles/Pong.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Pong.dir/link.d"
+  "CMakeFiles/Pong.dir/main.c.o"
+  "CMakeFiles/Pong.dir/main.c.o.d"
+  "Pong"
+  "Pong.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/Pong.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ RayLib/Standard/install.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+EXEPATH="$(pwd)/build/Pong"
+INSTALLPATH="$HOME/.local/share/applications"
+EXEC="Exec=$EXEPATH"
+
+printf "Installing Pong to:\n$INSTALLPATH"
+printf "Executable:\n$EXEPATH\n"
+
+echo  "$EXEC" >> Pong.desktop
+cp Pong.desktop $INSTALLPATH/Pong.desktop
+
+echo "Done."
